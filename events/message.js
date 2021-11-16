a9760079e7a926547d620fd0aeaa6d31c518d6f0
Michał Buda <michalbuda999@gmail.com>
2021-11-16 03:40:10 +0100

    added: message counter

--- events/message.js
+++ events/message.js
@@ -1,7 +1,18 @@
+const {createPool, createConnection} = require('mysql')
+
+const con = createConnection({
+    host: 'localhost',
+    user: 'harambowski',
+    password: 'jebacdisa',
+    database: 'node_test'
+})
+
 module.exports = {
     name: 'message',
     once: false,
     execute(message) {
         // console.log(`${message.author.tag} sent: ${message.content}`);
+        con.query(`UPDATE messages SET msgCount = msgCount + 1 WHERE user_id = ${message.author.id}`)
+
     }
 }
